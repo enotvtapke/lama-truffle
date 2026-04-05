7de30048e4a8695f9cf2de79809f697017043fe5
YuliaKorolenko <korollenkoyullia@gmail.com>
2026-04-05 17:10:40 +0400

    Add hash, tagHash, getEnv builtin functions
    Co-authored-by: enotvtapke <a.stupnikov05@gmail.com>

---
+++ language/src/main/java/com/oracle/truffle/sl/nodes/lama/builtin/LamaTagHashBuiltinNode.java
@@ -0,0 +1,17 @@
+package com.oracle.truffle.sl.nodes.lama.builtin;
+
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.nodes.NodeInfo;
+import com.oracle.truffle.sl.runtime.lama.LamaRuntimeLib;
+import com.oracle.truffle.sl.runtime.lama.LamaString;
+
+@NodeInfo(shortName = "tagHash")
+public abstract class LamaTagHashBuiltinNode extends LamaBuiltinNode {
+
+    @Specialization
+    @TruffleBoundary
+    public long hash(LamaString s) {
+        return s.hashCode();
+    }
+}
