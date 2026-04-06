ba490aca64863083df50ddae871f18ebb6fbba31
Aleksandr Stupnikov <a.stupnikov05@gmail.com>
2026-04-06 13:18:26 +0200

    Add toString for LamaNode
    Co-authored-by: YuliaKorolenko <korollenkoyullia@gmail.com>

--- language/src/main/java/com/oracle/truffle/sl/nodes/lama/LamaNode.java
+++ language/src/main/java/com/oracle/truffle/sl/nodes/lama/LamaNode.java
@@ -65,4 +65,34 @@ public abstract class LamaNode extends Node {
         }
         return source.createSection(sourceCharIndex, sourceLength);
     }
+
+    @Override
+    public String toString() {
+        return formatSourceSection(this);
+    }
+
+    /**
+     * Formats a source section of a node in human readable form. If no source section could be
+     * found it looks up the parent hierarchy until it finds a source section. Nodes where this was
+     * required append a <code>'~'</code> at the end.
+     *
+     * @param node the node to format.
+     * @return a formatted source section string
+     */
+    private static String formatSourceSection(Node node) {
+        SourceSection section = node.getSourceSection();
+        boolean estimated = false;
+        if (section == null) {
+            section = node.getEncapsulatingSourceSection();
+            estimated = true;
+        }
+
+        if (section == null || section.getSource() == null) {
+            return "<unknown source>";
+        } else {
+            String sourceName = section.getSource().getName();
+            int startLine = section.getStartLine();
+            return String.format("%s:%d%s", sourceName, startLine, estimated ? "~" : "");
+        }
+    }
 }
