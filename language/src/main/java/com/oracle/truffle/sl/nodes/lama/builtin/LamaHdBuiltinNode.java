f0abb655920ea4272b0ecfb41976b204aa395ceb
YuliaKorolenko <korollenkoyullia@gmail.com>
2026-04-09 10:30:10 +0400

    Remove from tl and hd builtins specialization for LamaArray
    Co-authored-by: enotvtapke <a.stupnikov05@gmail.com>

--- language/src/main/java/com/oracle/truffle/sl/nodes/lama/builtin/LamaHdBuiltinNode.java
+++ language/src/main/java/com/oracle/truffle/sl/nodes/lama/builtin/LamaHdBuiltinNode.java
@@ -4,24 +4,10 @@ import com.oracle.truffle.api.dsl.Fallback;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.nodes.NodeInfo;
 import com.oracle.truffle.sl.SLException;
-import com.oracle.truffle.sl.runtime.lama.LamaArray;
 import com.oracle.truffle.sl.runtime.lama.LamaSExpr;
 
 @NodeInfo(shortName = "hd")
 public abstract class LamaHdBuiltinNode extends LamaBuiltinNode {
-
-    @Specialization(guards = "value == 0")
-    public Object hdEmpty(@SuppressWarnings("unused") long value) {
-        throw SLException.create("hd: empty list", this);
-    }
-
-    @Specialization
-    public Object hdArray(LamaArray a) {
-        if (a.getSize() < 1) {
-            throw SLException.create("hd: empty array", this);
-        }
-        return a.readElement(0);
-    }
 
     @Specialization
     public Object hdCons(LamaSExpr e) {
